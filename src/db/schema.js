5bdc6fe48b9096795a1b93ac36e180ceb503d698
Keval4002 <88760664+Keval4002@users.noreply.github.com>
2026-02-23 18:08:27 +0530

    implement database schemas

---
+++ src/db/schema.js
@@ -0,0 +1,31 @@
+import {pgTable, serial, text, integer, timestamp, pgEnum, jsonb} from 'drizzle-orm/pg-core'
+
+export const matchEnumStatus =  pgEnum('match_status', ['scheduled', 'live', 'finished']);
+
+export const matches = pgTable("matches",{
+    id: serial('id').primaryKey(),
+    sport: text('sport').notNull(), 
+    homeTeam: text('home_team').notNull(), 
+    awayTeam: text('away_team').notNull(),
+    status: matchEnumStatus('status').notNull().default('scheduled'), 
+    startTime:timestamp('start_time'), 
+    endTime: timestamp('end_time'), 
+    homeScore: integer('home_score').notNull().default(0),
+    awayScore: integer('away_score').notNull().default(0), 
+    createdAt: timestamp('created_at').notNull().defaultNow(), 
+})
+
+export const commentry = pgTable("commentry",{
+    id: serial('id').primaryKey(), 
+    matchId: integer('match_id').notNull().references(()=>matches.id),
+    minute: integer('minute'),
+    sequence: integer('sequence'),
+    period: text('period'),
+    eventType: text('event_type'),
+    actor: text('actor'),
+    team: text('team'),
+    message: text('message').notNull(),
+    metadate: jsonb('metadata'),
+    tags: text('tage').array(),
+    createdAt: timestamp('created_at').notNull().defaultNow(),
+})
