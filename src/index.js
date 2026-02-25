08418637c40f88de41c80cf61ff9c650263045e8
Keval4002 <88760664+Keval4002@users.noreply.github.com>
2026-02-25 22:15:54 +0530

    implement websocket server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import express from 'express'
 import { matchRouter } from './routes/matches.js';
+import 'dotenv/config'
+import {attachWebsocketServer} from './ws/server.js'
+import http from 'http'
 
 const app = express();
 
-const port = 8000;
+const PORT = process.env.PORT||8000;
+const HOST = process.env.HOST||'0.0.0.0';
+
+const server = http.createServer(app);
 
 app.use(express.json());
 
 app.get('/', (req, res)=>{
     console.log("Hello from express server");
-})
+});
 
 app.use('/matches', matchRouter);
 
-app.listen(port, ()=>{
-    console.log(`Running server on port ${port}`);
+const {broadcastMatchCreated} = attachWebsocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
+
+server.listen(PORT, ()=>{
+    const baseUrl = HOST === '0.0.0.0'? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`
+    console.log(`Running server on ${baseUrl}`);
+    console.log(`Websocket running on ${baseUrl.replace('http', 'ws')}/ws`);
 })
